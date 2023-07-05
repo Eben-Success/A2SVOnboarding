e0f06236acac3b0431e2c1c213c3ff1798e0e4d2
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2023-07-05 14:56:30 +0000

    Time: 60 ms (6.74%), Space: 16.3 MB (72.89%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,45 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        """
+        1. Use for loop to group the 0s to the left.
+        2. Iterate over the array if num is > 0: break.
+        3. Set left pointer to i and right pointer to len(nums) -1
+        4. Swap if nums[right] is 1
+        """
+        
+        zero_index = 0
+        
+        for i in range(len(nums)):
+            if nums[i] == 0 and zero_index < len(nums):
+                nums[i], nums[zero_index] = nums[zero_index], nums[i]
+                zero_index += 1
+                
+        l, r = 0, len(nums) -1
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                l = i
+                break
+                
+        while l < r:
+            if nums[r] != 1:
+                r -= 1
+            else:
+                if nums[l] == 1:
+                    l += 1
+                else:
+                    nums[r], nums[l] = nums[l], nums[r]
+                    l += 1
+                    r -= 1
+                    
+                
+        
+                
+            
+        
+        
+                
+        
