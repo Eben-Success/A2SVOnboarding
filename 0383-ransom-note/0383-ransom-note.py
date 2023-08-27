b7534098c8204598bf8a8f6b2e9ced51cfb2b0e6
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2023-08-27 17:25:58 +0000

    Time: 79 ms (32.92%), Space: 16.6 MB (63.43%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,15 +1,31 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
 
-        hash_1 = defaultdict(int)
+        # hash_1 = defaultdict(int)
 
-        for char in magazine:
-            hash_1[char] += 1
+        # for char in magazine:
+        #     hash_1[char] += 1
+
+        # for char in ransomNote:
+        #     hash_1[char] -= 1
+
+        # return all(num >= 0 for num in hash_1.values())
+
+
+        rans = [0] * 26
+        mag = [0] * 26
 
         for char in ransomNote:
-            hash_1[char] -= 1
+            rans[ord(char) - ord('a')] += 1
 
-        return all(num >= 0 for num in hash_1.values())
+        for char in magazine:
+            mag[ord(char) - ord('a')] += 1
+
+
+        for i in range(26):
+            if mag[i] < rans[i]:
+                return False
+        return True
 
         
 
