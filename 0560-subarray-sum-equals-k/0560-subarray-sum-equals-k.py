d4883d30ee82e92f216ff02e409678d206529501
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2023-08-28 17:37:00 +0000

    Time: 254 ms (84.63%), Space: 19 MB (25.49%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,56 +1,19 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
 
-
-        # Optimal Approach
-        # Time: O(n) | Space: O(n) : len(hashmap)
-
-        #Edge case: Add 0 to the hashmap
         hashmap = {0: 1}
         count = 0
-        cur_sum = 0
+        prefix = 0
 
-        for i in range(len(nums)):
-            cur_sum += nums[i]
+        for num in nums:
+            prefix += num
 
-            diff = cur_sum - k
-            count += hashmap.get(diff, 0)
+            diff = prefix - k
 
-            hashmap[cur_sum] = 1 + hashmap.get(cur_sum, 0)
+            if diff in hashmap:
+                count += hashmap[diff]
+
+            hashmap[prefix] = 1 + hashmap.get(prefix, 0)
 
         return count
-
-
-
-
-
-
-
-        """
-        1. We set two pointers i and j.
-        2. Both pointers starts at the same index. (i==j)
-        3. Move j until total == k, and we increment count by 1.
-        4. Return count
-        """
-        # # Time: O(n^2) | Space: O(1)
-        # # Brute Force
-        # n = len(nums)
-        # total = 0
-        # count = 0
-        # for i in range(n):
-        #     total = 0
-        #     for j in range(i, n):
-        #         total += nums[j]
-        #         if total == k:
-        #             count += 1
-        # return count
-
-
-
-
-
-
-
         
-
-
