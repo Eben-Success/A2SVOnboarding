eee8151d2eb61601648124eae82c65c2890f9c82
Eben-Success <aebenezer237@gmail.com>
2023-09-03 17:34:24 +0000

    update

--- A2SV Camping/611.valid-triangle-number.py
+++ A2SV Camping/611.valid-triangle-number.py
@@ -5,8 +5,27 @@
 #
 
 # @lc code=start
+from typing import List
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
+        
+        count = 0
+        nums.sort()
+        n = len(nums)
+        
+        for i in range(2, n):
+            left = 0
+            right = i - 1
+            
+            while left < right:
+                if nums[left] + nums[right] > nums[i]:
+                    count += (right - left)
+                    right -= 1
+                    
+                else:
+                    left += 1
+                    
+        return count
         
 # @lc code=end
 
