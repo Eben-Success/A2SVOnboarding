90de670d39c16ebc5de008dfef386d3658261477
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2023-09-06 14:59:11 +0000

    Time: 38 ms (73.75%), Space: 16.3 MB (90.92%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -8,39 +8,28 @@ class Solution:
 
         dummy = ListNode(0)
         dummy.next = head
-        left_prev = dummy
+        l_prev = dummy
         cur = head
         idx = 0
 
         while cur and idx < left - 1:
             cur = cur.next
-            left_prev = left_prev.next
+            l_prev = l_prev.next
             idx += 1
 
         idx = 0
-        right_prev = None
+        r_prev = None
         while cur and idx < right - left + 1:
             nxt = cur.next
-            cur.next = right_prev
-            right_prev = cur
+            cur.next = r_prev
+            r_prev = cur
             cur = nxt
             idx += 1
 
-        left_prev.next.next = cur
-        left_prev.next = right_prev
+        l_prev.next.next = cur
+        l_prev.next = r_prev
 
         return dummy.next
-
-        
-
-
-
-        """
-        1. Set dummy node.
-        2. Iterate the list to left - 1
-        3. Reverse the list to right - left + 1
-        4. Reroute head and tail.
-        """
 
 
         
