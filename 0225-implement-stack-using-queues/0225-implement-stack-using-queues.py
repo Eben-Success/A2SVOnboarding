3f629ee6f6c9cede68dbf17c25bb837f50efa914
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2023-09-07 12:21:13 +0000

    Time: 41 ms (37.98%), Space: 16.4 MB (67.74%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,26 +1,3 @@
-# class MyStack:
-
-#     def __init__(self):
-#         self.stack = []
-        
-
-#     def push(self, x: int) -> None:
-#         self.stack.append(x)
-        
-
-#     def pop(self) -> int:
-#         if self.stack:
-#             return self.stack.pop()
-        
-
-#     def top(self) -> int:
-#         if self.stack:
-#             return self.stack[-1]
-        
-
-#     def empty(self) -> bool:
-#         return len(self.stack) == 0
-
 class ListNode:
     def __init__(self, val):
         self.val = val
@@ -29,54 +6,28 @@ class ListNode:
 class MyStack:
 
     def __init__(self):
-
-        self.head = None
+        self.dummy = ListNode(0)
         
+
     def push(self, x: int) -> None:
         newNode = ListNode(x)
 
-        if not self.head:
-            self.head = newNode
-            newNode = self.head
-            return
-
-        cur = self.head
-        while cur and cur.next:
-            cur = cur.next
-        cur.next = newNode
-
+        newNode.next = self.dummy.next
+        self.dummy.next = newNode
 
     def pop(self) -> int:
-        if not self.head:
-            return
-
-        cur = self.head
-        if cur.next is None:
-            self.head = None
-            return cur.val
-        else:
-            while cur.next and cur.next.next:
-                cur = cur.next
-            val = cur.next.val
-            cur.next = None
-            return val
+        nxt = self.dummy.next
+        self.dummy.next = nxt.next
+        nxt.next = None
+        return nxt.val
 
         
     def top(self) -> int:
-        if not self.head:
-            return
-        cur = self.head
-        while cur and cur.next:
-            cur = cur.next
-        return cur.val
-
+        return self.dummy.next.val
+        
 
     def empty(self) -> bool:
-        if not self.head:
-            return True
-        else:
-            return False
-
+        return not self.dummy.next
         
 
 
