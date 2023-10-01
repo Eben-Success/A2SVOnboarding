155e5ff335936594e9987ab80548d8d0a27d0b00
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2023-10-01 18:56:10 +0000

    Time: 973 ms (87.21%), Space: 30.3 MB (50.21%) - LeetHub

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+
+        if m * k > len(bloomDay):
+            return -1
+
+        l = min(bloomDay)
+        r = max(bloomDay)
+        res = -1
+
+        while l <= r:
+            mid = l + (r-l)//2
+            count,bouq = 0,0
+            for bloom in bloomDay:
+                if mid >= bloom:
+                    count += 1
+                else:
+                    bouq += count//k
+                    count = 0
+            bouq += count // k
+        
+            if bouq < m:
+                l = mid + 1
+            else:
+                res = mid
+                r = mid - 1
+
+        return res
+
+
+            
+        
