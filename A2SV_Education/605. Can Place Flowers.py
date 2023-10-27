905f4f700ba33319ffeea954b2b2385ca2b360d0
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2023-10-27 08:56:28 +0000

    Create 605. Can Place Flowers.py
    done

---
+++ A2SV_Education/605. Can Place Flowers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        res = [0] + flowerbed + [0]
+        print(res)
+
+        for i in range(1, len(res) -1):
+            if res[i] == 0:
+                if res[i-1] == 0 and res[i+1] == 0:
+                    res[i] = 1 
+                    n -= 1
+        return n <= 0
+
+
+
+        
