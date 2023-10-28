0f460fe463b59bd5a9805369960a1fb77eba30bb
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2023-10-28 12:34:10 +0000

    Create 77. Combinations.py
    done

---
+++ A2SV_Education/77. Combinations.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+        def is_valid_state(state):
+            return len(state) == k
+
+        def get_candidates(state):
+            if len(state) == 0:
+                res = set([i for i in range(1, n + 1)])
+            else:
+                res = set([i for i in range(max(list(state)), n + 1)])
+            return res.difference(state)
+
+        def search(state, solutions):
+            if is_valid_state(state):
+                solutions.append(state.copy())
+            # return
+
+            for candidate in get_candidates(state):
+                state.add(candidate)
+                search(state, solutions)
+                state.remove(candidate)
+
+        solutions = []
+        state = set()
+        search(state, solutions)
+        return solutions
