1e673d710ae11d0b2e7c8767decd4c018748674a
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2024-02-15 17:29:43 +0000

    Create 461. Hamming Distance.py

---
+++ A2SV_Education/bit_manipulation/461. Hamming Distance.py
@@ -0,0 +1,26 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+      
+
+        a = self.DecToBin(x)
+        b = self.DecToBin(y)
+
+        a = "0" * (max(len(a), len(b)) - len(a)) + a
+        b = "0" * (max(len(a), len(b)) - len(b)) + b
+
+        count = 0
+
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                count += 1
+
+        return count
+        
+    def DecToBin(self, n: int) -> int:
+        res = ""
+
+        while n:
+            res += str(n & 1)
+            n >>= 1
+        return res[::-1]
+        
