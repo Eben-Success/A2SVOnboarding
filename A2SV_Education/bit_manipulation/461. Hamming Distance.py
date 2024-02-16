b796cec9255e70c257dd6bd81c2f89e701834bd0
Ebenezer Acquah <71809927+Eben-Success@users.noreply.github.com>
2024-02-16 09:49:51 +0000

    Update 461. Hamming Distance.py

--- A2SV_Education/bit_manipulation/461. Hamming Distance.py
+++ A2SV_Education/bit_manipulation/461. Hamming Distance.py
@@ -1,26 +1,27 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
       
+    #     a = self.DecToBin(x)
+    #     b = self.DecToBin(y)
 
-        a = self.DecToBin(x)
-        b = self.DecToBin(y)
+    #     a = "0" * (max(len(a), len(b)) - len(a)) + a
+    #     b = "0" * (max(len(a), len(b)) - len(b)) + b
 
-        a = "0" * (max(len(a), len(b)) - len(a)) + a
-        b = "0" * (max(len(a), len(b)) - len(b)) + b
+    #     count = 0
 
-        count = 0
+    #     for i in range(len(a)):
+    #         if a[i] != b[i]:
+    #             count += 1
 
-        for i in range(len(a)):
-            if a[i] != b[i]:
-                count += 1
-
-        return count
+    #     return count
         
-    def DecToBin(self, n: int) -> int:
-        res = ""
+    # def DecToBin(self, n: int) -> int:
+    #     res = ""
 
-        while n:
-            res += str(n & 1)
-            n >>= 1
-        return res[::-1]
+    #     while n:
+    #         res += str(n & 1)
+    #         n >>= 1
+    #     return res[::-1]
+
+        return (x ^ y).bit_count()
         
